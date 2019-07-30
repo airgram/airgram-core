10d7da4b952d4f118ab64857d9a60c13b0ab252f
esindger <esindger@gmail.com>
2019-07-30 22:43:06 +0800

    Moved types to @airgram/core/types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import '../types'
 import { Airgram } from './Airgram'
 import { Composer, createContext, createState, Serializable, TDLibError, TdProvider } from './components'
 
-export * from '../types/api'
 export { Airgram, createContext, createState, Composer, Serializable, TDLibError, TdProvider }
